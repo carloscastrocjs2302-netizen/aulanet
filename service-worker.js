8793ea0ef6665ce936aa2ce3c155496f5133453f
carloscastrocjs2302-netizen <carloscastrocjs2302@gmail.com>
2026-08-21 14:23:37 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aulanet-v2';
+const CACHE_NAME = 'aulanet-v3';
 const APP_SHELL = [
   './',
   './index.html',
@@ -12,6 +12,8 @@ const APP_SHELL = [
   './login.js',
   './primer-ingreso.js',
   './dashboard-director.js',
+  './ficha-estudiante.js',
+  './informe-pdf.js',
   './dashboard-coordinador-area.js',
   './dashboard-admin.js',
   './carga-excel.js',
